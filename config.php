ceb20da00a09bb34690a9399dd2ef7f2214c88f4
pedroveras <pedroveras@gmail.com>
2013-05-07 05:04:54 -0300

    Changes in config

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+	define('APP_ID','');
+	define('HOST', '');
+	define('SECURE', '');
+	define('PORT', '');
+	define('EMAIL','');
+	define('USERNAME','');
+	define('PASSWORD','');
+	define('FROM','Ebay Cost Calculator');
+	define('SUBJECT_TO_CUSTOMER','Order Confirmation');
+	define('SUBJECT_USER_INFORMATION','Order Confirmation');
+	
+?>
