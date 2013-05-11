f86f4555bd45901c37f25a09906a8814e98a7ba9
Pedro Veras <pedroveras@gmail.com>
2013-05-10 23:01:40 -0300

    Confirmation page

---
+++ confirmation.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>EBAY COST CALCULATOR</title>
+<link type="text/css" rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+<div id="stylized" class="myform" style="overflow: auto;">
+	<h2>Order Confirmed</h2>
+	<p>Your order has been placed successfully! We've sent a copy of your order details to your email.</p>
+	<a href="index.php">Return to main page</a>	
+</div>
+</body>
+</html>
